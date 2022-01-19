7a3757af9b94765e6b8a96da7241bca167aa2964
Jason Cochran <jason.cochran@gmail.com>
2022-01-18 23:12:58 -0500

    update to ZMA airports and ATC positions

--- config/zab.js
+++ config/zab.js
@@ -1,6 +1,6 @@
 export default {
-	atcPos: ["PHX", "ABQ", "TUS", "AMA", "ROW", "ELP", "SDL", "CHD", "FFZ", "IWA", "DVT", "GEU", "GYR", "LUF", "RYN", "DMA", "FLG", "PRC", "AEG", "BIF", "HMN", "SAF", "FHU"],
-	airports: ["KPHX", "KABQ", "KTUS", "KAMA", "KROW", "KELP", "KSDL", "KCHD", "KFFZ", "KIWA", "KDVT", "KGEU", "KGYR", "KLUF", "KRYN", "KDMA", "KFLG", "KPRC", "KAEG", "KBIF", "KHMN", "KSAF", "KFHU"],
+	atcPos: ["MIA","ZMO","TPA","RSW","PBI","NQX","FLL","TMB","OPF","EYW","SRQ","6FA","APF","BCT","BKV","BOW","FMY","FPR","FXE","HST","LAL","MCF","PIE","PGD","PMP","SPG","SUA"],
+	airports: ["X06","07FA","06FA","KAVO","KAGR","KBOW","X10","KBCT","KBKV","X05","KCLW","2IS","X36","X01","KFLL","FXE","KFMY","KRSW","KFPR","KHWO","KHST","X51","KIMM","KEYW","KNQX","X14","X49","KLAL","X07","X25","KMTH","KMKY","KTMB","KTNT","KMIA","KOPF","KAPF","KOBE","KPHK","KPCM","KPMP","KSPG","KPIE","KSRQ","X26","KSEF","KSUA","KTPA","KVDF","KMCF","X39","KTPF","KVNC","X52","KVRB","KCHN","F45","KPBI","KLNA","KGIF","KZPH"],
 	ratings: ["Unknown", "OBS", "S1", "S2", "S3", "C1", "C2", "C3", "I1", "I2", "I3", "SUP", "ADM"],
 	ratingsLong: ["Unknown", "Observer", "Student", "Student 2", "Senior Student", "Controller", "Controller 2", "Senior Controller", "Instructor", "Instructor 2", "Senior Instructor", "Supervisor", "Administrator"],
 };
