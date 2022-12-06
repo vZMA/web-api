801ea2f2b6c03eca7bf1ef50930e2c296b0a59e6
Tom Coombs <tom@cube74.com>
2022-12-05 23:23:14 -0500

    secrets integration

--- middleware/vatsimOAuth.js
+++ middleware/vatsimOAuth.js
@@ -16,6 +16,7 @@ export default function (req, res, next) {
 
   const params = new URLSearchParams();
   params.append("grant_type", "authorization_code");
+  //Added secrets
   params.append("client_id", process.env.VATSIM_AUTH_CLIENT_ID);
   params.append("client_secret", process.env.VATSIM_AUTH_CLIENT_SECRET); 
   params.append("code", code);
