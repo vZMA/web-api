df06117835b240ada0a94e6de286da006fd91a57
Tom Coombs <tom@cube74.com>
2023-04-02 17:20:05 -0400

    surface google info

--- controllers/GoogleController.js
+++ controllers/GoogleController.js
@@ -6,8 +6,8 @@ import Config from '../models/Config.js';
 
 router.get('/googleinfo', async (req, res) => {
 	
-	console.log(clientId);
-	console.log(clientSecret);
+	console.log(googleClientId);
+	console.log(googleClientSecret);
 
 	res.stdRes.data = {
 		clientId: googleClientId,
