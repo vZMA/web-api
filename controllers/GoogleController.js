7544b6f75b405ab9c22e2723a36502e319431dd0
Tom Coombs <tom@cube74.com>
2023-04-02 15:57:33 -0400

    surface google connection info

---
+++ controllers/GoogleController.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import microAuth from '../middleware/microAuth.js';
+const router = express.Router();
+
+import User from '../models/User.js';
+import Config from '../models/Config.js';
+
+router.get('/googleinfo', microAuth, async (req, res) => {
+	
+	const clientId = process.env.GOOGLE_AUTH_CLIENT_ID;
+	const clientSecret = process.env.GOOGLE_AUTH_CLIENT_SECRET;
+	
+	res.stdRes.data = {
+		ClientId: clientId,
+		ClientSecret: clientSecret
+	}
+
+	console.log(res);
+	
+	return res.json(res.stdRes);
+});
+
+export default router;
