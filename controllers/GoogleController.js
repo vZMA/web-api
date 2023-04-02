5f550980c20f1a62ce2cd6215484da4f177f3dfa
Tom Coombs <tom@cube74.com>
2023-04-02 17:54:49 -0400

    surface google info

--- controllers/GoogleController.js
+++ controllers/GoogleController.js
@@ -1,16 +1,10 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import getGoogleClientInfo from '../helpers/googleInfoHelper.js';
 import getGoogleClientSecret from '../helpers/googleInfoHelper.js';
 
 const router = express.Router();
 
-import User from '../models/User.js';
-import Config from '../models/Config.js';
-
 router.get('/googleinfo', async (req, res) => {
-	dotenv.config();
-
 	const googleClientId = getGoogleClientInfo();
 	const googleClientSecret = getGoogleClientSecret();
 	console.log(googleClientId);
--- helpers/googleInfoHelper.js
+++ helpers/googleInfoHelper.js
@@ -2,16 +2,14 @@
 
 import axios from "axios";
 
-export default {
-  // Method utilizing access token from OAuth middle ware to retrieve user data needed to process a login from VATSIM API.
-  async getGoogleClientInfo() {
+export function etGoogleClientInfo() {
     const googleClientId = process.env.GOOGLE_AUTH_CLIENT_ID;
 
     return googleClientId;
-  },
-  async getGoogleClientSecret() {
+  };
+
+export function getGoogleClientSecret() {
     const googleClientSecret = process.env.GOOGLE_AUTH_CLIENT_SECRET;
 
     return googleClientSecret;
-  },
-}
+  };
